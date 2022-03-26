d338f36431d6b2ec126be754f062c22c0bf5fbf9
AlesandraIsla <alesandramempis@gmail.com>
2022-03-26 19:49:11 -0300

    Estilização da página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,42 @@
-import { GetStaticProps } from 'next';
-
-import { getPrismicClient } from '../services/prismic';
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Home() {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Exemplo</strong>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Alesandra Marinho
+                </li>
+              </ul>
+            </a>
+          </Link>
+        </div>
+      </main>
+    </>
+  );
 }
-
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
-
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-// export default function Home() {
-//   // TODO
-// }
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
